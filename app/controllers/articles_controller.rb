ded0f56809060b4d2c468d06588b701b65086bf0
saidMontufar <said.montufar@outlook.com>
2022-12-27 13:14:13 -0500

    Refactor views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+  # --> Before running any of the actions listed will run the function set_article
+  # --> %i allows to write non-interpolated arrays separated by white spaces
+  before_action :set_article, only: %i[show edit update destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,12 +15,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article =
-      Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
@@ -27,8 +28,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was saved successfully"
       redirect_to @article
     else
@@ -37,8 +37,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private #---> Anything bellow is a priavte method to this controller
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
